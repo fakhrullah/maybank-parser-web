8f48ae43701a1189db3464ac7c495864fb2b1603
Fakhrullah Padzil <fakhrullah@fajarhac.com>
2022-07-23 04:11:38 +0800

    Add click action on button

--- src/ButtonSecondary.jsx
+++ src/ButtonSecondary.jsx
@@ -1,8 +1,9 @@
-function ButtonSecondary({className = "", children}) {
+function ButtonSecondary({ className = "", children, onClick }) {
     const defaultClasses = "m-1 px-5 py-1 rounded-md bg-gray-100 text-gray-500 shadow-gray-600 border hover:bg-gray-200";
     return (
         <button
             class={`${defaultClasses} ${className}`}
+            onClick={onClick}
         >
             {children}
         </button>
